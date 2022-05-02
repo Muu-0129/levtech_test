e710ac0280871cd086c4165c623bb7213290ea99
Muu-0129 <ayumumurataka@gmail.com>
2022-05-03 01:09:37 +0900

    22.05.03

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+use Illuminate\Support\Facades\Log;     //debug
+
+/*
+8-4 PostRequest.php作成後
+作成前は汎用性のあるRequest: use Illuminate\Http\Request;
+*/
 
 class PostController extends Controller
 {
@@ -19,6 +25,18 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return view('show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('create');
+    }
+    
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
         
     }
 }
